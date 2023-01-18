e138d1497d633d880c355ab236a26ad55b5b55c5
Wemerson <wemersonedx@gmail.com>
2023-01-18 10:07:00 -0300

    Add project files.

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class BaseRepository<T> where T : BaseModel
+    {
+        protected readonly ApplicationContext context;
+        protected readonly DbSet<T> dbSet;
+
+
+        public BaseRepository(ApplicationContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<T>();
+        }
+    }
+}
---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -0,0 +1,69 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IPedidoRepository
+    {
+        void AddItem(string codigo);
+        Pedido GetPedido();
+    }
+    public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
+    {
+
+        private readonly IHttpContextAccessor contextAccessor;
+        public PedidoRepository(ApplicationContext context, IHttpContextAccessor contextAccessor) : base(context)
+        {
+            this.contextAccessor = contextAccessor;
+        }
+
+        public void AddItem(string codigo)
+        {
+            var produto = context.Set<Produto>()
+                .Where(p => p.Codigo == codigo).FirstOrDefault();
+            if (produto == null)
+            {
+                throw new ArgumentException("Produto não encontrado");
+            }
+            var pedido = GetPedido();
+            var itemPedido = context.Set<ItemPedido>()
+                .Where(i => i.Produto.Codigo == codigo && i.Pedido.Id == pedido.Id).SingleOrDefault();
+
+            if(itemPedido == null)
+            {
+                itemPedido = new ItemPedido(pedido, produto, 1, produto.Preco);
+                context.Set<ItemPedido>().Add(itemPedido);
+                context.SaveChanges();
+            }
+
+        }
+
+        public Pedido GetPedido()
+        {
+            var pedidoId = GetPedidoId();
+            var pedido = dbSet.Where(p => p.Id == pedidoId).SingleOrDefault();
+
+            if(pedido == null)
+            {
+                pedido = new Pedido();
+                dbSet.Add(pedido);
+                context.SaveChanges();
+                SetPedidoId(pedido.Id);
+            }
+            return pedido;
+
+        }
+
+        private int? GetPedidoId()
+        {
+            return contextAccessor.HttpContext.Session.GetInt32("pedidoId");            
+        }
+
+        private void SetPedidoId(int pedidoId)
+        {
+            contextAccessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);
+        }
+    }
+}
